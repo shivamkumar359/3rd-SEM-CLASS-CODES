f7e96d08dc2d2f044006d1d8ffd8897688b94826
Shivam_kumar359 <shivamkumarbarnwal359@gmail.com>
2026-09-04 09:46:40 +0530

    Fix calculation of average and update count logic
    Calculate the sum of numbers greater than 25 instead of counting them.

--- week1.1.py
+++ week1.1.py
@@ -1,6 +1,9 @@
+# Sum of numbers greater than 25 in tempt
+
 tempt = [30,32,54,21,16,25,44,78]
-avg = sum(tempt/len(tempt))
+avg = sum(tempt)/len(tempt)
 count = 0
-for t in tempt : 
-  if t>33:
-    count+= 1
+for t in tempt :
+  if t>25:
+    count+= t
+print(count)
