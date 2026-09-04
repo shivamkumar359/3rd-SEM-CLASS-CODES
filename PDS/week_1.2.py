87064195f686e90867fe74ca86c1e1ac6703d00f
Shivam_kumar359 <shivamkumarbarnwal359@gmail.com>
2026-09-04 14:56:33 +0530

    Remove Python program to count even and odd numbersRE

--- PDS/week_1.2.py
+++ PDS/week_1.2.py
@@ -1,18 +1,1 @@
-# Python projgramm to count even and odd in list 
 
-#list of numbers 
-list = [1,4,5,2,45,65,78,14,21,35,99,77,28,68]
-even_count, odd_count = 0,0
-
-# Iterating each number in list 
-for num in list :
-
-  #checking condition 
-  if num % 2 == 0:
-    even_count += 1
-
-  else :
-    odd_count += 1
-
-print("Even number in list of numbers are :" , even_count)
-print("Odd number in list of numbrs are : " , odd_count)
